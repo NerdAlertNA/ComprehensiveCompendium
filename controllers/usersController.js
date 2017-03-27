d5a993789fcc20e1a57a9cf9887dad8601f279b3
jacob <jacob.butler1994@gmail.com>
2017-03-27 14:28:21 -0700

    added a new user rout with a working submit form.

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -14,4 +14,24 @@ router.get('/', function(req, res){
     });
 });
 
+router.get('/new', function(req, res) {
+	res.render('users/new.hbs')
+});
+
+router.post('/', function(req, res) {
+	var user = new User({
+		username: req.body.username,
+		password: req.body.password
+	});
+	user.save(function(err, user){
+		if (err) {console.log(err); }
+		console.log(user);
+		res.redirect('/users')
+	});
+});
+
+
+
+
+
 module.exports = router;
